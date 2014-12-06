42aeeb5b90b616f4536a9fc8b40a26d40d0edc19
Czar Dixon <rbheromax@gmail.com>
2014-12-06 05:40:42 -0500

    Filesystem hacks, Mem map improvements

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export ARCH=arm
 export CROSS_COMPILE=../arm-eabi-4.6/bin/arm-eabi-
-make clean
-make a11chl_defconfig
-make all CONFIG_DEBUG_SECTION_MISMATCH=y CONFIG_NO_ERROR_ON_MISMATCH=y
+# make clean
+# make a11chl_defconfig
+make all CONFIG_DEBUG_SECTION_MISMATCH=y CONFIG_NO_ERROR_ON_MISMATCH=y CONFIG_LOCALVERSION=
+# make modules CONFIG_DEBUG_SECTION_MISMATCH=y CONFIG_NO_ERROR_ON_MISMATCH=y CONFIG_LOCALVERSION=
 bin/make.sh
 echo "Image is at:"
 echo "boot.img"
